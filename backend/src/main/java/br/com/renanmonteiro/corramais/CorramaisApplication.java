17e45053215ccea88ec766aeb516851a7de98eef
Renan Monteiro <renan.paula@pmrun.com.br>
2024-06-06 16:19:35 -0300

    first commit backend

---
+++ backend/src/main/java/br/com/renanmonteiro/corramais/CorramaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.renanmonteiro.corramais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorramaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorramaisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/renanmonteiro/corramais/CorramaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renanmonteiro.corramais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorramaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
