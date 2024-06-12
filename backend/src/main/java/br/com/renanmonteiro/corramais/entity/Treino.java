e37b7e2b1c7d4bc03a4e041c19f50b929de6233e
Renan Monteiro <renan.paula@pmrun.com.br>
2024-06-12 05:36:02 -0300

    created entity Treino

---
+++ backend/src/main/java/br/com/renanmonteiro/corramais/entity/Treino.java
@@ -0,0 +1,71 @@
+package br.com.renanmonteiro.corramais.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "treino")
+public class Treino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private String nome;
+
+    private int distancia;
+    
+    private String tempo;
+
+    public Treino(String descricao, String nome, int distancia, String tempo) {
+        this.descricao = descricao;
+        this.nome = nome;
+        this.distancia = distancia;
+        this.tempo = tempo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public String getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(String tempo) {
+        this.tempo = tempo;
+    }
+}
